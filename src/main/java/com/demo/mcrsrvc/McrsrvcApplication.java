4c8034e220ea0838dd3cc332c4662344ca6de44b
batainem96 <batainem96@gmail.com>
2022-04-07 23:11:34 -0500

    Initial commit.

---
+++ src/main/java/com/demo/mcrsrvc/McrsrvcApplication.java
@@ -0,0 +1,13 @@
+package com.demo.mcrsrvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McrsrvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McrsrvcApplication.class, args);
+	}
+
+}
